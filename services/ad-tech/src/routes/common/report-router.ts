87d7948a81fa7f0404a59c58816b98dae2d7f954
cwserna <cwserna@google.com>
2025-03-14 17:00:22 -0600

    Revert "Adding a route to clear report cache for demo and debugging"

--- services/ad-tech/src/routes/common/report-router.ts
+++ services/ad-tech/src/routes/common/report-router.ts
@@ -133,12 +133,3 @@ ReportRouter.get('/view-reports', async (req: Request, res: Response) => {
     ...hostDetails,
   });
 });
-
-ReportRouter.get('/clean-reports', async (req: Request, res: Response) => {
-  const hostDetails = getTemplateVariables('Clear Report Cache');
-
-  res.render('clean-reports', {
-    reports: ReportStore.cleanReports(),
-    ...hostDetails,
-  });
-});
